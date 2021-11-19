4af4a234172834381a4b8fb4a9ebc62d5d99d2dc
Tismont <80092794+Tismont@users.noreply.github.com>
2021-11-19 13:53:25 +0100

    Add files via upload

---
+++ logIn.php
@@ -0,0 +1,25 @@
+<?php
+$conn = new mysqli('****', '****', '****', '****');
+    
+if($conn === false){
+    die("ERROR: Could not connect. " 
+        . mysqli_connect_error());
+}
+    
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+
+$sql = "SELECT * FROM userstable WHERE username = ('$username') AND password = ('$password')";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo 'Hello ' . $row['username'];
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
+?>
